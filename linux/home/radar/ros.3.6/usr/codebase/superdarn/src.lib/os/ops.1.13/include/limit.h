715bb35f87e0e499c1b403ba6371188da41edea7
ksterne <kevintyler@vt.edu>
2016-07-15 14:38:33 +0000

    Increased iq buffer size for pcode ops

--- linux/home/radar/ros.3.6/usr/codebase/superdarn/src.lib/os/ops.1.13/include/limit.h
+++ linux/home/radar/ros.3.6/usr/codebase/superdarn/src.lib/os/ops.1.13/include/limit.h
@@ -5,26 +5,26 @@
 
 /*
  LICENSE AND DISCLAIMER
- 
+
  Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- 
+
  This file is part of the Radar Software Toolkit (RST).
- 
+
  RST is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  any later version.
- 
+
  RST is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
- 
+
+
+
 */
 
 
@@ -34,12 +34,14 @@
 
 #define ORIGIN_TIME_SIZE 26
 #define ORIGIN_COMMAND_SIZE 128
-#define PULSE_SIZE 64    
-#define LAG_SIZE 300            
-#define MAX_RANGE 700
-#define COMBF_SIZE 80       
+#define PULSE_SIZE 64
+#define LAG_SIZE 300
+/* Increased range from 700 */
+#define MAX_RANGE 800
+#define COMBF_SIZE 80
 #define MAX_BEAM 255
 #define MAXNAVE 300
-#define IQBUFSIZE 524288
+/* Doubled the size of IQBUFSIZE for pcode ops */
+#define IQBUFSIZE 1048576
 #endif
 
