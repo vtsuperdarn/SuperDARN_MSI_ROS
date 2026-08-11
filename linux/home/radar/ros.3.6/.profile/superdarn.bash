b1259c9d4717c7cee94f59376cc9a091fcb30301
Kevin Sterne <kevintyler@vt.edu>
2026-08-11 19:51:45 +0000

    Eliminating a DOS formatted end character, updates from radar

--- linux/home/radar/ros.3.6/.profile/superdarn.bash
+++ linux/home/radar/ros.3.6/.profile/superdarn.bash
@@ -17,11 +17,11 @@ export SD_MODEL_TABLE=${RSTPATH}"/tables/superdarn/model"
 #                                                                   #
 #####################################################################
 
-export SD_ERRLOG_PATH="/data/ros/errlog";
+export SD_RAW_PATH="/data/ros/rawacf"
 export SD_FIT_PATH="/data/ros/fitacf"
 export SD_IQ_PATH="/data/ros/iqdat"
-export SD_RAW_PATH="/data/ros/rawacf"
-export SD_SND_PATH="/data/ros/snd"
+export SD_ERRLOG_PATH="/data/ros/errlog"
 
-
+# For normalsound control program
+export SD_SND_PATH="/data/ros/snd/"
 
